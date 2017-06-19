b742a51413351d1c03bc7f4fddd2c7d8acfd59e9
Dylan Orzel <dylan.orzel@gmail.com>
2017-06-19 14:19:34 -0600

    url evaluation error was using an unassigned variable in exception output

--- http_requests_base.py
+++ http_requests_base.py
@@ -72,7 +72,7 @@ class HTTPRequestsBase(Retry, EnrichSignals, Block):
             url = self.url(signal)
         except Exception as e:
             self.logger.warning(
-                "Failed to evaluate url {}: {}".format(url, e)
+                "Failed to evaluate url {}: {}".format(self.url(), e)
             )
             return
         timeout = self.timeout(signal) if self.timeout(signal) else None
