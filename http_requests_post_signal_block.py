bcc35b1034c177e796024f9a36d16dd1fff0a95d
neutralioJenkins <jenkins@n.io>
2018-06-19 16:35:23 -0600

    Bump version to v0.2.0

--- http_requests_post_signal_block.py
+++ http_requests_post_signal_block.py
@@ -15,7 +15,7 @@ class HTTPRequestsPostSignal(HTTPRequestsBase):
         http_method (select): HTTP method (ex. GET, POST,
             PUT, DELETE, etc).
     """
-    version = VersionProperty("0.1.2")
+    version = VersionProperty("0.2.0")
     http_method = SelectProperty(
         HTTPMethod,
         default=HTTPMethod.POST,
